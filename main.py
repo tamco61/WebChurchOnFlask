196413abfa0b3c5fd82aa2764547c1ff57267b6c
tam <tamco@list.ru>
2020-04-09 15:34:03 +0500

    фикс таблиц

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ class RegisterForm(FlaskForm):
     email = StringField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    surname = StringField('Фамилия пользователя', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
     submit = SubmitField('Зарегиcтрироваться')
 
 
